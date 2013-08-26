b241af15432b45d6d52df8cbb36a6aa6b8271bb3
aksyanov <aksyanov.r@gmail.com>
2013-08-26 12:18:11 +0300

    ok

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$yii=dirname(__FILE__).'/framework/yii.php';
+$yii=dirname(__FILE__).'/framework/yii.php' ;
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following line when in production mode
